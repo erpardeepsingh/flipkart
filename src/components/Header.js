53a0a9ec881d4efe902e2a6079f44364cff3fc71
erpardeepsingh <itspardeepsingh@gmail.com>
2019-06-04 19:57:42 +0530

    =k

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+    <div className="App">
+        <div id="container">
+         <div data-reactroot="">
+            <div class="_3ybBIU">
+               <div class="_1tz-RS">
+                  <div class="_1S7OK2">
+                  </div>
+                  <div class="_3pNZKl">
+                     <div class="_1T-JyI">
+                        <div class="_3dGepu">
+                           <a href="index.html">
+                           <img width="75" src="../img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_4ee2f9.png" alt="Flipkart" title="Flipkart" class="_1e_EAo"/></a>
+                           <a class="_33x-Wq" href="plus.html">
+                              Explore <span class="_2Ky4Ru">Plus</span><img width="10" src="../img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_b13a8b.png"/>
+                           </a>
+                        </div>
+                     </div>
+                     <div class="Y5-ZPI">
+                        <form class="_1WMLwI header-form-search" action="https://www.flipkart.com/search" method="GET">
+                           <div class="col-12-12 _2tVp4j">
+                              <div class="O8ZS_U"><input type="text" value="" class="LM6RPg" title="Search for products, brands and more" name="q" autoComplete="off" placeholder="Search for products, brands and more"/></div>
+                              <button class="vh79eN" type="submit">
+                                 <svg width="20" height="20" viewBox="0 0 17 18" class="" xmlns="http://www.w3.org/2000/svg">
+                                    <g fill="#2874F1" fill-rule="evenodd">
+                                       <path class="_2BhAHa" d="m11.618 9.897l4.225 4.212c.092.092.101.232.02.313l-1.465 1.46c-.081.081-.221.072-.314-.02l-4.216-4.203"></path>
+                                       <path class="_2BhAHa" d="m6.486 10.901c-2.42 0-4.381-1.956-4.381-4.368 0-2.413 1.961-4.369 4.381-4.369 2.42 0 4.381 1.956 4.381 4.369 0 2.413-1.961 4.368-4.381 4.368m0-10.835c-3.582 0-6.486 2.895-6.486 6.467 0 3.572 2.904 6.467 6.486 6.467 3.582 0 6.486-2.895 6.486-6.467 0-3.572-2.904-6.467-6.486-6.467">
+                                       </path>
+                                    </g>
+                                 </svg>
+                              </button>
+                              <input type="hidden" name="otracker" value="search"/><input type="hidden" name="otracker1" value="search"/><input type="hidden" name="marketplace" value="FLIPKART"/><input type="hidden" name="as-show" value="off"/><input type="hidden" name="as" value="off"/>
+                           </div>
+                           <ul class="col-12-12 _1PBbw8"></ul>
+                        </form>
+                     </div>
+                     <div class="_1jA3uo _34niwY">
+                        <div class="dHGf8H"><a href="account/login83ce.html?ret=/search%3Fsid%3Dfcn%26otracker%3DCLP_Filters%26p%255B%255D%3Dfacets.serviceability%255B%255D%253Dtrue%26p%255B%255D%3Dfacets.brand%255B%255D%253DSennheiser">Login &amp; Signup</a></div>
+                     </div>
+                     <div class="_1jA3uo">
+                        <div class="dHGf8H">
+                           <div class="_1jcwFN _3dmQRh">
+                              <span>
+                                 <div class="_2aUbKa">
+                                    More 
+                                 </div>
+                              </span>
+                           </div>
+                           <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1">
+                              <path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" class="_14jSvk"></path>
+                           </svg>
+                        </div>
+                     </div>
+                     <div class="_1jA3uo">
+                        <a class="_3ko_Ud" href="https://www.flipkart.com/viewcart?otracker=Cart_Icon_Click">
+                           <svg class="_2fcmoV" width="14" height="14" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                              <path class="_2JpNOH" d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86" fill="#fff"></path>
+                           </svg>
+                           <span>Cart</span>
+                        </a>
+                     </div>
+                  </div></div></div></div></div></div> 
+        )
+  }
+}
+export default Header;
